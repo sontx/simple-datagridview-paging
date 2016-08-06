0965770e610086c003eb37dd4a259069fa346980
xuan son tran <xuanson33bk@gmail.com>
2016-08-06 23:05:45 +0700

    + add test for auto mode(soft mode)

---
+++ Test/Form3.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Test
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPaging1 = new SimpleDataGridViewPaging.DataGridViewPaging();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPaging1
+            // 
+            this.dataGridViewPaging1.AutoHideNavigator = false;
+            this.dataGridViewPaging1.DataSource = null;
+            this.dataGridViewPaging1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewPaging1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewPaging1.MaxRecords = 100;
+            this.dataGridViewPaging1.Name = "dataGridViewPaging1";
+            this.dataGridViewPaging1.ReadOnly = false;
+            this.dataGridViewPaging1.Size = new System.Drawing.Size(374, 281);
+            this.dataGridViewPaging1.TabIndex = 0;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 281);
+            this.Controls.Add(this.dataGridViewPaging1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SimpleDataGridViewPaging.DataGridViewPaging dataGridViewPaging1;
+    }
+}
---
+++ Test/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form3 : Form
+    {
+        private SQLiteConnection connection;
+
+        public Form3()
+        {
+            InitializeComponent();
+            connection = new SQLiteConnection("Data Source=chinook.db");
+            connection.Open();
+
+            dataGridViewPaging1.UserSoftMode(connection, "SELECT Name, Composer, Bytes FROM tracks WHERE TrackId < 500");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            connection.Dispose();
+        }
+    }
+}
