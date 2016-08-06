3e7ec2d1d8a810d73b4cd9f64b14b0e737f36e0d
xuan son tran <xuanson33bk@gmail.com>
2016-08-06 22:11:24 +0700

    + add test for auto mode(hard mode)

---
+++ Test/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form2 : Form
+    {
+        private SQLiteConnection connection;
+
+        public Form2()
+        {
+            InitializeComponent();
+            connection = new SQLiteConnection("Data Source=chinook.db");
+            connection.Open();
+
+            dataGridViewPaging1.UserHardMode(connection, "tracks");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            connection.Dispose();
+        }
+    }
+}
