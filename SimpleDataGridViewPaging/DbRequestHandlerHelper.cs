c20e40fa7d7feb2034326437e9d3ca8d5ce1eca7
Son Xuan <Son.Xuan.Tran@mgm-tp.com>
2019-04-26 14:07:31 +0700

    * throw exception when uses with sql server database

--- SimpleDataGridViewPaging/DbRequestHandlerHelper.cs
+++ SimpleDataGridViewPaging/DbRequestHandlerHelper.cs
@@ -1,27 +1,41 @@
 ﻿using System.Data.Common;
+using System.Data.SqlClient;
 using Code4Bugs.SimpleDataGridViewPaging.Statement.Builder;
 
 namespace Code4Bugs.SimpleDataGridViewPaging
 {
     public static class DbRequestHandlerHelper
     {
+        private static readonly string SqlServerSelectCommandText = "SELECT * FROM {0} ORDER BY (SELECT NULL) OFFSET {2} ROWS FETCH NEXT {1} ROWS ONLY";
+
         public static IDbRequestHandler Create(DbConnection connection, string tableName)
         {
-            return new DbRequestHandler
-            {
-                Connection = connection,
-                TableName = tableName
-            };
-        }
+            var statementBuilder = connection.GetType() == typeof(SqlConnection)
+                ? new RowsStatementBuilder().CommandText(SqlServerSelectCommandText) as RowsStatementBuilder
+                : null;
 
-        public static IDbRequestHandler Create(DbConnection connection, string tableName,
-            DbCommandBuilder commandBuilder)
-        {
             return new DbRequestHandler
             {
                 Connection = connection,
                 TableName = tableName,
-                RowsStatementBuilder = new UpdatableRowsStatementBuilder().CommandBuilder(commandBuilder)
+                RowsStatementBuilder = statementBuilder
+            };
+        }
+
+        public static IDbRequestHandler Create(
+            DbConnection connection,
+            string tableName,
+            DbCommandBuilder commandBuilder)
+        {
+            var statementBuilder = new UpdatableRowsStatementBuilder().CommandBuilder(commandBuilder);
+            if (connection.GetType() == typeof(SqlConnection))
+                statementBuilder.CommandText(SqlServerSelectCommandText);
+
+            return new DbRequestHandler
+            {
+                Connection = connection,
+                TableName = tableName,
+                RowsStatementBuilder = statementBuilder
             };
         }
     }
